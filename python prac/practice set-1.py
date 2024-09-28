2070640b2516bded3373a43cf075c421edcd0cc1
yuvaarajj <ryuva125@gmail.com>
2024-09-28 18:22:32 +0530

    Initial commit

---
+++ python prac/practice set-1.py
@@ -0,0 +1,48 @@
+# Q - 1
+print('''
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
+      ''')
+
+# Q - 3 
+
+import emoji
+
+result = emoji.emojize('Python is :thumbs_up')
+print(result)
+
+# Q - 4
+
+import os
+
+# Specify the directory you want to list
+directory = './'
+
+# Get the list of files and directories
+contents = os.listdir(directory)
+
+
+print(contents)
+
---
+++ python prac/practiceset-2.py
@@ -0,0 +1,32 @@
+# Q - 1
+
+# a = 1
+# b = 2
+# print(a + b)
+
+# Q - 2
+
+# z = 5
+# print(2%z)
+
+# Q - 3
+
+# q = input()
+# print(type(q))
+
+# Q - 4
+
+# a = 34
+# b = 80
+# print(a > b)
+
+# Q - 5
+
+# a = int(input())
+# b = int(input())
+# print(a + b / 2)
+
+# Q - 6
+
+a = int(input())
+print(a ** 2)
