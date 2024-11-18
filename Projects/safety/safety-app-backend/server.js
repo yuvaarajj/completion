82362e3953ae2c2e6950c41bc2d9cd454f7a2064
yuvaarajj <ryuva125@gmail.com>
2024-11-18 14:04:01 +0530

    Update server.js

--- Projects/safety/safety-app-backend/server.js
+++ Projects/safety/safety-app-backend/server.js
@@ -1,27 +1,34 @@
-const express = require('express');
+const express = require("express");
 
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
-const locationRoutes = require('./routes/locationRoutes');
-
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const locationRoutes = require("./routes/locationRoutes");
 
 dotenv.config();
 
 const app = express();
-app.use('/api/location', locationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/location", locationRoutes);
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+app.get('/', (req, res) => {
+  res.send('Welcome to the Safety App Backend!');
+});
+
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
